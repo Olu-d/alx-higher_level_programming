e33137f869f8f47fa7b85f94264fcf60ad3a0442
Olu-D <tunsmen13@icloud.com>
2023-01-06 23:06:02 +0000

    Python intro

---
+++ 0x00-python-hello-world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+str = "Holbertron School"
+print(f"{str*3}")
+print(f"{str:9}")
---
+++ 0x00-python-hello-world/9-easter_egg_copy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+str = (f"The Zen of Python, by Tim Peters"
+
+f" Beautiful is better than ugly."
+f"Explicit is better than implicit."
+f"Simple is better than complex."
+f"Complex is better than complicated."
+f"Flat is better than nested."
+f"Sparse is better than dense."
+f"Readability counts."
+f"Special cases aren't special enough to break the rules."
+f"Although practicality beats purity."
+f"Errors should never pass silently."
+f"Unless explicitly silenced."
+f"In the face of ambiguity, refuse the temptation to guess."
+f"There should be one-- and preferably only one --obvious way to do it."
+f"Although that way may not be obvious at first unless you're Dutch."
+f"Now is better than never."
+f"Although never is often better than *right* now."
+f"If the implementation is hard to explain, it's a bad idea."
+f"If the implementation is easy to explain, it may be a good idea."
+f"Namespaces are one honking great idea -- let's do more of those!")
+print(str)
