57d311148c2f1b4d1a843e463ea58206699bc69c
Olu-D <tunsmen13@icloud.com>
2023-01-08 22:43:57 +0000

    pycodestyle correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 i = 0
-
 while i <= 9:
     for j in range(i + 1, 10):
         if (i != 8):
-            print("{}{}, ".format(i,j), end="")
+            print("{}{}, ".format(i, j), end="")
         else:
             print("{}{}".format(i, j))
     i += 1
-
